683ccd6d7b13d313339f8d7953ed9f22cbaf3db2
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2017-07-14 20:49:24 +0200

    Add 'Charts' and 'RealmSwift' dependency

--- Podfile
+++ Podfile
@@ -3,4 +3,6 @@ use_frameworks!
 
 target 'bitcoin' do
     pod 'Alamofire', '~> 4.4'
+    pod 'Charts', '~> 3.0.2'
+    pod 'RealmSwift', '~> 2.8.3'
 end
---
+++ bitcoin/bitcoin-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  bitcoin-Bridging-Header.h
+//  bitcoin
+//
+//  Created by Filipe Santos Correa on 14.07.17.
+//  Copyright © 2017 Filipe Santos Correa. All rights reserved.
+//
+
+#ifndef bitcoin_Bridging_Header_h
+#define bitcoin_Bridging_Header_h
+
+
+#endif /* bitcoin_Bridging_Header_h */
