ea6fb2ea458b91e5782adadd1ad50475100a5149
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2017-07-14 18:04:58 +0200

    Initial Commit

---
+++ bitcoin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bitcoin
+//
+//  Created by Filipe Santos Correa on 14.07.17.
+//  Copyright © 2017 Filipe Santos Correa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
